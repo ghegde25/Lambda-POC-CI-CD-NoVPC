428abab75ece8f617f3850fa253ff608d47a6bd8
ghegde25 <112864031+ghegde25@users.noreply.github.com>
2022-09-09 20:31:05 +0530

    5

--- lambda_function.py
+++ lambda_function.py
@@ -7,17 +7,12 @@ s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     bucket ='lambdapocs3put'
-
 	transactionToUpload = {}
 	transactionToUpload['transactionId'] = '5008'
 	transactionToUpload['type'] = 'PURCHASE'
 	transactionToUpload['amount'] = 200
-	transactionToUpload['customerId'] = 'H-23567'
-
-	fileName = 'H-23567' + '.json'
-
+	transactionToUpload['customerId'] = 'CID-11111'
+	fileName = 'CID-11111' + '.json'
 	uploadByteStream = bytes(json.dumps(transactionToUpload).encode('UTF-8'))
-
 	s3.put_object(Bucket=bucket, Key=fileName, Body=uploadByteStream)
-
 	print('Put Complete')
